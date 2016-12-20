1a9322feff245e5887065f8ece0fcd19a79bf136
Ted Henley <thenley@users.noreply.github.com>
2016-12-20 09:49:00 -0800

    Delete SetNetworksPrivate.ps1

--- SAHA/SetNetworksPrivate/SetNetworksPrivate.ps1
+++
@@ -1,5 +0,0 @@
-﻿#Get network connections 
-$networkConnections = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}")).GetNetworkConnections() 
-
-#Set network location to Private for all networks 
-$networkConnections | % {$_.GetNetwork().SetCategory(1)} 
